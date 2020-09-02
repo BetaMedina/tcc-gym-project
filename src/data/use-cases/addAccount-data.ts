6fedb1efc3cc98689144283d1d6850f80a117d96
BetaMedina <joaopedromedinadeandrade@gmail.com>
2020-09-02 20:29:41 -0300

    addAccountUseCase

---
+++ src/data/use-cases/addAccount-data.ts
@@ -0,0 +1,11 @@
+import { UserAccount } from '../../domain/models/use-account'
+import { AddAccountRepository } from '../../domain/use-cases/add-account-db'
+import { AddAccount, AddAccountReceived } from '../protocols/add-account'
+
+export class DbAddAccount implements AddAccount {
+  constructor (private addAccountReposity :AddAccountRepository) {}
+
+  async create (accountData:AddAccountReceived):Promise<UserAccount> {
+    return this.addAccountReposity.create(accountData)
+  }
+}

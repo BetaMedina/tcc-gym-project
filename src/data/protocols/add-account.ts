e6f192874f557af6a9aa54ded6d7f4d1ee83dd13
BetaMedina <joaopedromedinadeandrade@gmail.com>
2020-09-02 20:28:53 -0300

    criando add-account

---
+++ src/data/protocols/add-account.ts
@@ -0,0 +1,11 @@
+import { UserAccount } from '../../domain/models/use-account'
+
+export interface AddAccountReceived{
+  name:string,
+  email:string,
+  password:string
+}
+
+export interface AddAccount{
+  create (account:AddAccountReceived):Promise<UserAccount> 
+}

1ad4b10cb0a10a0866716c37c035dc9dcc424d97
BetaMedina <joaopedromedinadeandrade@gmail.com>
2020-09-02 20:28:31 -0300

    criando signup controller

---
+++ src/presentation/controllers/signUp/signUp-controller.ts
@@ -0,0 +1,23 @@
+import { ServerError } from '../../errors'
+import { badRequest, serverError, successResponse } from '../../helpers/http/http-helper'
+import { Validation, HttpRequest, HttpResponse, ControllerInterface, AddAccount } from './signUp-protocols'
+
+export class SignUp implements ControllerInterface {
+  constructor (
+    private readonly payloadValidation:Validation,
+    private readonly addAccount:AddAccount
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const error = await this.payloadValidation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      await this.addAccount.create(httpRequest.body)
+      return successResponse('User created with success')
+    } catch (err) {
+      return serverError(new ServerError(err))
+    }
+  }
+}

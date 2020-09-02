63bd26ed2bce96fc9a121256a6e8517a06dabd39
BetaMedina <joaopedromedinadeandrade@gmail.com>
2020-09-02 20:29:07 -0300

    criando signUp protocols

---
+++ src/presentation/controllers/signUp/signUp-protocols.ts
@@ -0,0 +1,5 @@
+export * from '../../protocols'
+export * from '../../../domain/use-cases/add-account-db'
+export * from '../../../domain/models/use-account'
+export * from '../../protocols/validation'
+export * from '../../../data/protocols/add-account'
